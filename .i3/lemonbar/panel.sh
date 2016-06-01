71d6a03e29a851dbb8ee5e8c2ff4344416bb0ff6
Guy Nankivell <guy.nankivell@gmail.com>
2016-06-01 15:56:17 +1200

    This adds the initial custom files. Hated the prev names

--- .i3/lemonbar/panel.sh
+++ .i3/lemonbar/panel.sh
@@ -25,7 +25,6 @@ $(dirname $0)/workspaces.pl > "${panel_fifo}" &
 
 # IRC, "IRC"
 # only for init
-~/bin/irc_warn &
 
 # Conky, "SYS"
 conky -c $(dirname $0)/conky > "${panel_fifo}" &
@@ -43,18 +42,6 @@ while :; do
     cnt_vol=0
   fi
 
-  # GMAIL, "GMA"
-  if [ $((cnt_mail++)) -ge ${upd_mail} ]; then
-    printf "%s%s\n" "GMA" "$(~/bin/gmail.sh)" > "${panel_fifo}"
-    cnt_mail=0
-  fi
-
-  # MPD
-  if [ $((cnt_mpd++)) -ge ${upd_mpd} ]; then
-    #printf "%s%s\n" "MPD" "$(ncmpcpp --now-playing '{%a - %t}|{%f}' | head -c 60)" > "${panel_fifo}"
-    printf "%s%s\n" "MPD" "$(mpc current -f '[[%artist% - ]%title%]|[%file%]' 2>&1 | head -c 70)" > "${panel_fifo}"
-    cnt_mpd=0
-  fi
 
   # Finally, wait 1 second
   sleep 1s;
